f085f721a2d8a2947afbfd1933cc61a44d5dd895
Razibuddin Sozol <44525991+RazibuddinSozol@users.noreply.github.com>
2020-06-29 04:19:19 +0600

    Add files via upload

---
+++ Uva-10079.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cmath>
+#include<cstdio>
+#include<cstring>
+using namespace std;
+int main()
+{
+
+    long long b,a;
+    int t,tc;
+    while(scanf("%lld",&a)==1){
+        cout<<a*(a+1)/2+1<<endl;
+    }
+    return 0;
+}
